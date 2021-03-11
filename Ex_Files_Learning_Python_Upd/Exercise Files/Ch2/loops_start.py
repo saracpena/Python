f5c48fdb9ab1dbc7dadc6d7a90d916ad31bf181e
Sara Peña <s.cristal.p@gmail.com>
2021-03-10 20:51:02 -0500

    Update loops_start.py

--- Ex_Files_Learning_Python_Upd/Exercise Files/Ch2/loops_start.py
+++ Ex_Files_Learning_Python_Upd/Exercise Files/Ch2/loops_start.py
@@ -6,18 +6,30 @@ def main():
   x = 0
 
   # define a while loop
-
+  # while(x < 5):
+  #   print(x)
+  #   x = x + 1
 
   # define a for loop
-
+  # for x in range(5, 10):
+  #   print(x)
+  # ! i.e. in JS for (i=0; i<10; i++)
 
   # use a for loop over a collection
-
+  # days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+  # for d in days:
+  #   print(d)
   
   # use the break and continue statements
-
+  for x in range(5, 10):
+    # if (x == 7): break
+    # if (x % 2 == 0): continue
+    # print(x)
 
   #using the enumerate() function to get index 
-  
+  days = ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]
+  for i, d in enumerate(days):
+    print (i, d)  
+
 if __name__ == "__main__":
   main()
