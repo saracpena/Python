f9b18d6cb4fd06c898ea678d6041d6fa9f7bb981
Sara Peña <s.cristal.p@gmail.com>
2021-03-10 19:57:54 -0500

    Update functions_start.py

--- Ex_Files_Learning_Python_Upd/Exercise Files/Ch2/functions_start.py
+++ Ex_Files_Learning_Python_Upd/Exercise Files/Ch2/functions_start.py
@@ -3,7 +3,8 @@
 #
 
 # define a basic function
-
+def func1():
+    print("I am a function")
 
 # function that takes arguments
 
@@ -15,5 +16,8 @@
 
 
 #function with variable number of arguments
+func1()
+print (func1())
+print (func1)
 
 
