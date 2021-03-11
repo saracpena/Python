b552f0b1a8194cb008a70ffd72659d456a3ce782
Sara Peña <s.cristal.p@gmail.com>
2021-03-10 21:07:15 -0500

    Classes & Modules

--- Ex_Files_Learning_Python_Upd/Exercise Files/Ch2/classes_start.py
+++ Ex_Files_Learning_Python_Upd/Exercise Files/Ch2/classes_start.py
@@ -2,10 +2,34 @@
 # Example file for working with classes
 #
 
+class myClass():
+   def method1(self):
+       print("myClass method1")
+
+   def method2(self, someString):
+        print("myClass mehtod2 " + someString)
+
+class anotherClass(myClass):
+    def method1(self):
+        print("anotherClass method1")
+    
+    def method2(self, someString):
+        print("anotherClass method2")
+
 def main():
-    print("Hello World")
-    name = input("What is your name? ")
-    print("Nice to meet you", name)
+    c = myClass()
+    c.method1()
+    c.method2("This is a string")
+
+    c2=anotherClass()
+    c2.method1()
+    c2.method2("This is a string")
+
+
+    # def main():
+    #     print("Hello World")
+    #     name = input("What is your name? ")
+    #     print("Nice to meet you", name)
 
 if __name__ == "__main__":
     main()
